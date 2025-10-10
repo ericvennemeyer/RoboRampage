d2dffa80d3beda4a336d21226ce48b8504a79c3e
Eric Vennemeyer <henri.barun@gmail.com>
2025-10-10 15:02:27 -0700

    4.6 -- Custom Reticles, 4.7 -- Advanced Jumping

--- Player/player.gd
+++ Player/player.gd
@@ -2,7 +2,9 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+@export var jump_height: float = 1.0
+@export var fall_multiplier: float = 2.5
 
 var mouse_motion: Vector2 = Vector2.ZERO
 
@@ -10,21 +12,21 @@ var mouse_motion: Vector2 = Vector2.ZERO
 
 
 func _ready() -> void:
-	var gravity = get_gravity()
-	print(gravity)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
 
 func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y >= 0:
+			velocity += get_gravity() * delta
+		else:
+			velocity += get_gravity() * delta * fall_multiplier
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * -get_gravity().y)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
