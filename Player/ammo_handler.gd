e86b7ea8e4b0ee630a3315681a6297e64e76294a
Eric Vennemeyer <henri.barun@gmail.com>
2025-11-04 10:05:37 -0800

    4.24 -- Tracking Ammo

---
+++ Player/ammo_handler.gd
@@ -0,0 +1,23 @@
+class_name AmmoHandler
+extends Node
+
+
+enum ammo_type {
+	BULLET,
+	SMALL_BULLET
+}
+
+var ammo_storage: Dictionary[int, int] = {
+	ammo_type.BULLET: 10,
+	ammo_type.SMALL_BULLET: 60
+}
+
+
+func has_ammo(type: ammo_type) -> bool:
+	return ammo_storage[type] > 0
+
+
+func use_ammo(type: ammo_type) -> void:
+	if has_ammo(type):
+		ammo_storage[type] -= 1
+		printt(type, ammo_storage[type])
--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -8,6 +8,8 @@ extends Node3D
 @export var muzzle_flash: GPUParticles3D
 @export var sparks: PackedScene
 @export var is_automatic: bool = false
+@export var ammo_handler: AmmoHandler
+@export var ammo_type: AmmoHandler.ammo_type
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
@@ -31,13 +33,15 @@ func _process(delta: float) -> void:
 
 
 func shoot() -> void:
-	cooldown_timer.start(1.0 / fire_rate)
-	muzzle_flash.restart()
-	weapon_mesh.position.z += recoil
-	var collider = ray_cast_3d.get_collider()
-	printt("Weapon Fired!", collider)
-	if collider is Enemy:
-		collider.hitpoints -= weapon_damage
-	var spark = sparks.instantiate()
-	add_child(spark)
-	spark.global_position = ray_cast_3d.get_collision_point()
+	if ammo_handler.has_ammo(ammo_type):
+		ammo_handler.use_ammo(ammo_type)
+		cooldown_timer.start(1.0 / fire_rate)
+		muzzle_flash.restart()
+		weapon_mesh.position.z += recoil
+		var collider = ray_cast_3d.get_collider()
+		printt("Weapon Fired!", collider)
+		if collider is Enemy:
+			collider.hitpoints -= weapon_damage
+		var spark = sparks.instantiate()
+		add_child(spark)
+		spark.global_position = ray_cast_3d.get_collision_point()
